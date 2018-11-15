36912d14348c08da52d3d55451c99749a5b0d91e
Arne B. Kråkenes <arnkr@outlook.com>
2018-11-15 21:39:07 +0100

    Enabled restart of application

--- javascript/automath.js
+++ javascript/automath.js
@@ -2,14 +2,18 @@
 function register() {
     let taskDiv = document.getElementById("task");
     let appDiv = document.getElementById("application");
-    let remainingTime = 600;
+    let remainingTime = 0;
     let score = 0;
     let successCount = 0;
     let failCount = 0;
+    let timerId = undefined;
     function setupTask() {
-        let comb = Math.floor(Math.random() * 100) + 1;
-        let left = Math.floor(comb / 10);
-        let right = comb % 10;
+        let comb = Math.floor(Math.random() * 100) + 10;
+        let a = Math.floor(comb / 10);
+        let b = comb % 10;
+        let swapped = Math.random() >= 0.5;
+        let left = swapped ? b : a;
+        let right = swapped ? a : b;
         let answer = left + right;
         taskDiv.setAttribute("answer", answer.toString());
         appDiv.setAttribute("class", "active");
@@ -25,16 +29,21 @@ function register() {
         timeDiv.innerText = (remainingTime / 10).toFixed(1);
     }
     function startTimer() {
-        setInterval(function () {
+        remainingTime = 600;
+        timerId = setInterval(function () {
             if (appDiv.getAttribute("class") == "active" && remainingTime > 0) {
                 remainingTime -= 1;
                 updateTime();
                 if (remainingTime == 0) {
+                    clearInterval(timerId);
+                    timerId = undefined;
                     appDiv.setAttribute("class", "inactive");
                     let scoreDiv = document.getElementById("score");
                     scoreDiv.setAttribute("class", "score-hilite");
                     setTimeout(function () {
                         scoreDiv.setAttribute("class", "score");
+                        appDiv.setAttribute("class", "ready");
+                        taskDiv.innerText = "? + ? =";
                     }, 2500);
                 }
             }
@@ -71,10 +80,17 @@ function register() {
             }, 2000);
         });
     }
-    updateScore();
-    updateTime();
-    startTimer();
-    setupTask();
+    document.getElementById("start").addEventListener("click", function () {
+        if (appDiv.getAttribute("class") != "ready")
+            return;
+        score = 0;
+        successCount = 0;
+        failCount = 0;
+        updateScore();
+        updateTime();
+        startTimer();
+        setupTask();
+    });
 }
 if (document.getElementById("a1") != null) {
     register();
